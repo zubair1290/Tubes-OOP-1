e8f6f199da36cb8d1d5a2a43fb4dddeca3257872
raffifpr <68497910+raffifpr@users.noreply.github.com>
2021-03-25 21:30:51 +0700

    kisi kisi tambahan :v

--- src/Skill.cpp
+++ src/Skill.cpp
@@ -1,68 +1,93 @@
-#include "Skill.h"
-
-Skill::Skill() {}
-
-Skill::~Skill() {}
-
-
+#include "SkillEngimon.h"
 #include <iostream>
+#include "Element.h"
 
 using namespace std;
 
 SkillEngimon :: SkillEngimon(){
 	this->basePower = 0;
 	this->masteryLvl = 1;
-	// this->Skill = "NULL";
-	this->jmlElmt = 0;
-	this->elements = new char[this->jmlElmt];
+	this->Skill = "NULL";
+	this->jmlElmt = 0
+	this->elements = new Element[this->jmlElmt];
 }
 SkillEngimon :: SkillEngimon(char Skill, int basePower, int masteryLvl, int jmlElmt){
 	this->basePower = basePower;
 	this->masteryLvl = masteryLvl;
 	this->Skill = Skill;
 	this->jmlElmt = jmlElmt;
-	this->elements = new char[this->jmlElmt];
+	this->elements = new Element[this->jmlElmt];
 }
 SkillEngimon :: SkillEngimon(const SkillEngimon& s){
-	this->basePower = s.basePower;
-	this->masteryLvl = s.masteryLvl;
-	this->Skill = s.Skill;
-	this->elements = new char [s.jmlElmt];
-	for (int i = 0; i < s.jmlElmt; i++){
-		this->elements[i] = s.elements[i];
+	this->basePower = s->basePower;
+	this->masteryLvl = s->masteryLvl;
+	this->Skill = s->Skill;
+	this->elements = new Element [s->jmlElmt];
+	for (i = 0; i < s->jmlElmt; i++){
+		this->elements[i] = s->elements[i];
 	}
 }
 SkillEngimon :: ~SkillEngimon(){
 	delete this;
 }
 
-// char SkillEngimon :: GetElements(){
-// 	return this->elements;
-// }
-
-void SkillEngimon :: SetElements(char Elmt){
-	this->elements[this->jmlElmt] = Elmt;
-	this->jmlElmt++;
+Element SkillEngimon :: GetElements( int x){
+	if (x < this->jmlElmt){
+		return this->elements[x];
+	}
 }
 
-// void learn(SkillEngimon S, Engimon E){
-// 	if (isFull()){
-// 		throw (LearnExp(SLOT_SKILL_FULL));
-// 	}else {
-// 		if (isThereSameElmt(S, E)){
-// 			addSkillToEngimon();
-// 		}else{
-// 			throw (LearnExp(NOT_SAME_ELMT));
-// 		}
-// 	}
-// }
+void SkillEngimon :: SetElements(Element E){
+	this->elements[this->jmlElmt] = E;
+	this->jmlElmt++
+}
 
-// void learnSkillItem(SkillEngimon S, Inventory I){
-// 	if (isFullInventory(I)){
-// 		throw (LearnExp(INVENTORY_FULL));
-// 	}else{
-// 		for (i = 0; i < I->capacity; i++){
-// 			if 
-// 		}
-// 	}
-// }
+bool SkillEngimon :: isThereSameElmt(SkillEngimon& S, Engimon E){
+	bool found = false;
+	for (i = 0; i < S->jmlElmt; i++){
+		if (GetElements(i) == getElement(E)){
+			found = true;
+		}
+	}
+	return found;
+}
+
+//void learn(SkillEngimon S, Engimon E){
+//	if (isFull()){
+//		throw (LearnExp(SLOT_SKILL_FULL));
+//	}else {
+//		if (isThereSameElmt(S, E)){
+//			if (isThereSameSkill(S, E)){
+//				throw (LearnExp(THERE_IS_SAME_SKILL));
+//			}else{
+//				addSkillToEngimon(S, E);
+//			}
+//		}else{
+//			throw (LearnExp(NOT_SAME_ELMT));
+//		}
+//	}
+//}
+void replaceSkillEngimon(SkillEngimon S, Engimon E){
+	char comfim;
+	int x;
+	cout << "Replace? (Y/N) : ";
+	cin >> confirm;
+	if (confirm == "Y"){
+		showEngimonSkills(E);
+		cout << "Skill yang ingin direposisi (1/2/3/4):";
+		cin >> x;
+		E->Skill[x-1] = S;
+		cout << "Reposisi Skill selesai ...";
+	}
+}
+
+//void learnSkillItem(SkillEngimon S, Inventory I){
+//	if (isFullInventory(I)){
+//		throw (LearnExp(INVENTORY_FULL));
+//	}else{
+		//cari nama skill yang sesuai dalam inventory dan jumlahnya tambahkan satu
+		//for (i = 0; i < I->capacity; i++){
+			//if 
+		}
+	}
+}
