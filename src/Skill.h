fb9285727ea4a06b0240baf8ee5f4c20d696637c
raffifpr <68497910+raffifpr@users.noreply.github.com>
2021-03-25 20:32:08 +0700

    Update Skill.h

--- src/Skill.h
+++ src/Skill.h
@@ -8,4 +8,24 @@ public:
     ~Skill();
 };
 
+class SkillEngimon {
+	private :
+		char Skill;
+		int basePower;
+		int masteryLvl;
+		char *elements;
+		int jmlElmt;
+	public :
+		SkillEngimon();
+		SkillEngimon(char Skill, int basePower, int masteryLvl, int jmlElmt);
+		SkillEngimon(const SkillEngimon&) s;
+		~SkillEngimon();
+		
+		char GetElements();
+		
+		void SetElements(char Elmt);
+		
+		void learn(SkillEngimon S, Engimon E);
+};
+
 #endif
