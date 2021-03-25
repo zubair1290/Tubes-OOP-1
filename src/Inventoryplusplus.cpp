aaad4a4e5920571227e675abd7f1a20e3e08a357
farhanfadillahr <farhan.fadillahrafi@gmail.com>
2021-03-25 23:10:28 +0700

    Invent sementara

---
+++ src/Inventoryplusplus.cpp
@@ -0,0 +1,68 @@
+#include "Inventoryplusplus.h"
+
+template <class E>
+Inventoryplusplus<E>::Inventoryplusplus(): Inventoryplusplus(15){
+    item = new E[this->size];
+}
+
+template <class E>
+Inventoryplusplus<E>::Inventoryplusplus(int _size): size(_size), countItem(0){
+    item = new E[this->size];
+}
+
+template <class E>
+Inventoryplusplus<E>& Inventoryplusplus<E>::operator<<(const E& I){
+    this->countItem = this->countItem+1;
+    E[this->countItem-1] = I;
+    return *this;
+}
+
+template <class E>
+int Inventoryplusplus<E>::getCountItem(){
+    return countItem;
+}
+
+template <class E>
+E Inventoryplusplus<E>::getItem(){
+    return item;
+}
+// ---------------------------------------Specialization-------------------------------------
+// template <>
+// Inventoryplusplus<Skill>::Inventoryplusplus(): Inventoryplusplus(15){
+//     item = new Skill[this->size];
+// }
+
+// template <>
+// Inventoryplusplus<Skill>::Inventoryplusplus(int _size): size(_size), countItem(0){
+//     item = new Skill[this->size];
+// }
+
+// template <>
+// Inventoryplusplus<Skill>& Inventoryplusplus<Skill>::operator<<(const Skill& I){
+//     this->countItem = this->countItem+1;
+//     int i = 0;
+//     bool ada = false;
+//     while(i < countItem && not ada){
+//         if(item[i] == I){
+//             ada = true;
+//         }else{
+//             ada = false;
+//             i++;
+//         }
+//     }
+//     if(ada){
+//         item[this->countItem-1] = I;
+
+//     }
+//     return *this;
+// }
+
+// template <>
+// int Inventoryplusplus<Skill>::getCountItem(){
+//     return countItem;
+// }
+
+// template <>
+// Skill Inventoryplusplus<Skill>::getItem(){
+//     return item;
+// }
