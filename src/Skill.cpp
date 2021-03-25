6f6d9bc2484ad62f6ac548cc08cd12a9ca05b01c
raffifpr <68497910+raffifpr@users.noreply.github.com>
2021-03-25 20:33:41 +0700

    Update Skill.cpp

--- src/Skill.cpp
+++ src/Skill.cpp
@@ -3,3 +3,66 @@
 Skill::Skill() {}
 
 Skill::~Skill() {}
+
+
+#include <iostream>
+
+using namespace std;
+
+SkillEngimon :: SkillEngimon(){
+	this->basePower = 0;
+	this->masteryLvl = 1;
+	this->Skill = "NULL";
+	this->jmlElmt = 0
+	this->elements = new char[this->jmlElmt];
+}
+SkillEngimon :: SkillEngimon(char Skill, int basePower, int masteryLvl, int jmlElmt){
+	this->basePower = basePower;
+	this->masteryLvl = masteryLvl;
+	this->Skill = Skill;
+	this->jmlElmt = jmlElmt;
+	this->elements = new char[this->jmlElmt];
+}
+SkillEngimon :: SkillEngimon(const SkillEngimon& s){
+	this->basePower = s->basePower;
+	this->masteryLvl = s->masteryLvl;
+	this->Skill = s->Skill;
+	this->elements = new char [s->jmlElmt];
+	for (i = 0; i < s->jmlElmt; i++){
+		this->elements[i] = s->elements[i];
+	}
+}
+SkillEngimon :: ~SkillEngimon(){
+	delete this;
+}
+
+char SkillEngimon :: GetElements(){
+	return this->elements;
+}
+
+void SkillEngimon :: SetElements(char Elmt){
+	this->elements[this->jmlElmt] = Elmt;
+	this->jmlElmt++
+}
+
+void learn(SkillEngimon S, Engimon E){
+	if (isFull()){
+		throw (LearnExp(SLOT_SKILL_FULL));
+	}else {
+		if (isThereSameElmt(S, E)){
+			addSkillToEngimon();
+		}else{
+			throw (LearnExp(NOT_SAME_ELMT));
+		}
+	}
+}
+
+void learnSkillItem(SkillEngimon S, Inventory I){
+	if (isFullInventory(I)){
+		throw (LearnExp(INVENTORY_FULL));
+	}else{
+		for (i = 0; i < I->capacity; i++){
+			if 
+		}
+	}
+}
